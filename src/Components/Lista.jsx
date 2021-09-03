a2d3f74c0cc788a4afdf74d83875aaac1b05de9a
Diego Vergara <d.v.petricio@live.com>
2021-09-03 19:52:00 -0400

    reproduce solo falta forward y backward

--- src/Components/Lista.jsx
+++ src/Components/Lista.jsx
@@ -1,42 +1,51 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faBackward, faForward, faPause } from '@fortawesome/free-solid-svg-icons';
-/*              */
+
 const Lista = () => {
     const [listaMusica, setLista] = useState([
         { "id": 1, "category": "game", "name": "Mario Castle", "url": "files/mario/songs/castle.mp3" },
         { "id": 2, "category": "game", "name": "Mario Star", "url": "files/mario/songs/hurry-starman.mp3" },
         { "id": 3, "category": "game", "name": "Mario Overworld", "url": "files/mario/songs/overworld.mp3" }
     ])
-    const [music, setMusic] = useState()
 
-    const listItems = listaMusica.map(function (lista, i) {
-        return <li key={i}><button onClick={setMusic(lista.url)}>{lista.name}</button></li>
+    let reproductor = useRef();
+
+    const listItems = listaMusica.map(function (cancion, i) {
+        const url = cancion.url
+        return <li key={i}><button onClick={() => {reproductor.src = "https://assets.breatheco.de/apis/sound/" + url}}>{cancion.name}</button></li>
     })
 
-    const reproductor = useRef();
+    useEffect(() => {
+        console.log(reproductor)
+    })
 
     const [icon, cambiarIcon] = useState(<FontAwesomeIcon icon={faPlay} />)
 
     function playAudio() {
-        if (reproductor.current.paused) {
-            reproductor.current.play();
+        if (reproductor.paused) {
+            reproductor.play();
             cambiarIcon(<FontAwesomeIcon icon={faPause} />);
         } else {
-            reproductor.current.pause();
+            reproductor.pause();
             cambiarIcon(<FontAwesomeIcon icon={faPlay} />);
+        }
+    }
+
+    function siguienteCancion() {
+        if (reproductor) {
+
         }
     }
 
     return (
         <div>
+            
             <ul>
                 {listItems}
             </ul>
             <div id="barra">
-                <audio ref={reproductor}>
-                    <source src={"https://assets.breatheco.de/apis/sound/" + { music }} type="audio/mpeg" />
-                </audio>
+                <audio ref={t => reproductor = t}></audio>
                 <div id="contBotones">
                     <button id="anterior" className="botones" /* onClick={ } */>
                         <FontAwesomeIcon icon={faBackward} />
